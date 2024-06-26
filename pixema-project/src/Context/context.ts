124b8cdd3bf3d43df3e61025910012288234caa7
nastyahh <121236405+nastyahh@users.noreply.github.com>
2024-06-26 18:10:07 +0300

    Delete pixema-project directory

--- pixema-project/src/Context/context.ts
+++
@@ -1,8 +0,0 @@
-import { createContext } from "react";
-import { IActiveContext, IThemeContext } from "../utility/types";
-
-export const ActiveContext = createContext<IActiveContext | undefined>(
-  undefined
-);
-
-export const ThemeContext = createContext<IThemeContext | undefined>(undefined);
--- pixema-project/src/hook/useAuth.ts
+++
@@ -1,7 +0,0 @@
-import { useContext } from "react";
-import { AuthContext } from "../HOC/AuthProvider";
-import { IAuthContext } from "../utility/types";
-
-export function useAuth() {
-  return useContext(AuthContext) as IAuthContext;
-}
